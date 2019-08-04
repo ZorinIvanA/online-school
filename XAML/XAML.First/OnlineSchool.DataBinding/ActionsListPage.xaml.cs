daf23532be3ef9ac45e8df3853b6544f99239e25
ZorinIvanA <zorin.ivan@outlook.com>
2019-08-04 21:18:55 +0300

    массивы

--- XAML/XAML.First/OnlineSchool.DataBinding/ActionsListPage.xaml.cs
+++ XAML/XAML.First/OnlineSchool.DataBinding/ActionsListPage.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -14,16 +13,16 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace OnlineSchool.DataBinding
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ActionPage : Page
+    public sealed partial class ActionsListPage : Page
     {
-        public ActionPage()
+        public ActionsListPage()
         {
             this.InitializeComponent();
         }
---
+++ XAML/XAML.First/OnlineSchool.DataBinding/ActionsListViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineSchool.DataBinding
+{
+    public class ActionsListViewModel
+    {
+        public ActionViewModel[] Actions { get; set; }
+        public int[,] Actions2 { get; set; }
+
+        public ActionsListViewModel()
+        {
+            Actions2 = new int[4, 7];
+            Actions2[3,1] = 2; 
+
+            Actions = new ActionViewModel[3];
+            Actions[0] = new ActionViewModel
+            {
+                ActionName = "Действие 1",
+                CompleteDate = DateTime.Parse("2050-01-01")
+            };
+            Actions[1] = new ActionViewModel
+            {
+                ActionName = "Действие 2",
+                CompleteDate = DateTime.Now
+            };
+            Actions[2] = new ActionViewModel
+            {
+                ActionName = "Действие 3",
+                CompleteDate = new DateTime(1900, 1, 1)
+            };
+        }
+    }
+}
